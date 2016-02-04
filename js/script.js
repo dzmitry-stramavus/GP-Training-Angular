bb4d016678ff05a57c14e3cbb4e2d72bc065e6ee
Dmitry Stremous <stremous@list.ru>
2016-02-04 16:47:12 +0300

    create a new feature with underscore library
    First, change was to create a html structure for view
    Second, we need to inject underscore features to angularJs
    Third, write a controller and function which multiplies every element in initial array.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 ;(function(){
-  var app = angular.module("calculusApp", []);
+  var underscore = angular.module('underscore', []);
+  underscore.factory('_', ['$window', function($window) {
+    return $window._; // assumes underscore has already been loaded on the page
+  }]);
+
+  var app = angular.module("calculusApp", ['underscore']);
 
   app.controller("getSumCtrl", function($scope){
 
+  });
+
+  app.controller("underscoreCtrl", function($scope, _){
+    $scope.array = [1, 2, 3, 4, 5];
+    $scope.arrayResult = $scope.array;
+
+    $scope.multiply = function(multiplyBy) {
+      $scope.arrayResult = _.map($scope.array, function(elem){
+        return elem * multiplyBy;
+      });
+      $scope.multiplyBy = "";
+    };
   });
 })();
