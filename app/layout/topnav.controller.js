3e3181a3a5f1fa0ab1b20b178f81e6a68879bc1e
Dmitry Stremous <stremous@list.ru>
2016-02-10 20:52:10 +0300

    Lesson3: rectify code + find two issues

--- app/layout/topnav.controller.js
+++ app/layout/topnav.controller.js
@@ -8,6 +8,10 @@ angular
 function topNav($scope, $route) {
   $scope.isTrue = true;
   $scope.isRoute = function(path) {
+    /*//issue: isRoute() is running every time several times, but controller is OK
+    console.log(path);
+    console.log($route.current.originalPath);
+    console.log($route.current.originalPath.substr(1, path.length) === path);*/
     return $route.current.originalPath.substr(1, path.length) === path;
   }
 }
--- app/sum/sum.controller.js
+++ app/sum/sum.controller.js
@@ -7,6 +7,7 @@ angular
 
 
 function sum($scope, $routeParams, $location, $http) {
+  console.log("sum controller");
   $scope.firstNumber = $routeParams.firstNumber || 0;
   $scope.secondNumber = $routeParams.secondNumber || 0;
 
@@ -15,22 +16,27 @@ function sum($scope, $routeParams, $location, $http) {
   };
 
   $scope.$watch('result + $parent.currency', function() {
+    console.log("wathcer: result + parent currency is running");
     $scope.converted = ($scope.$parent.data ? $scope.$parent.currency : 1) * $scope.result;
   });
 
+// issue: if we change path by $location.path, sum controller is running every time when inputs(firstNumber and secondNumber) are changed
   $scope.$watch('firstNumber', function(newValue, oldValue) {
+    console.log("watcher: first number");
     if (newValue !== oldValue) {
       $scope.$parent.focus = '1';
       $location.path( '/sum/' + $scope.firstNumber + '/plus/' + $scope.secondNumber);
     }
   });
   $scope.$watch('secondNumber', function(newValue, oldValue) {
+    console.log("watcher: secondNumber");
     if (newValue !== oldValue) {
       $scope.$parent.focus = '2';
       $location.path( '/sum/' + $scope.firstNumber + '/plus/' + $scope.secondNumber);
     }
   });
   $scope.$watch('focus', function(newValue, oldValue) {
+    console.log("watcher: focus is running");
     if (newValue === '1') {
       document.querySelector('#firstNumber').focus();
     } else if (newValue === "2") {
